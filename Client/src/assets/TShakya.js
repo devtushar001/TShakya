860603e5da4cedd1b5b1b643a34963ca8c37f1de
devtushar001 <tc341269@gmail.com>
2025-02-13 22:29:03 -0800

    new one

---
+++ Client/src/assets/TShakya.js
@@ -0,0 +1,72 @@
+import large from './large.png';
+import leap from './boy.png';
+import range from './range.png';
+import shuffle from './shuffle.png';
+import sort_descending from './sort-descending.png';
+import swap from './swap.png';
+import thermometer from './thermometer.png';
+import timer_icon from './fast-time.png'
+import cNet from './cNet.png';
+import app_store_icon from './app-store.png'
+import clc_md from './CalculateFactorial.txt';
+import add_icon from './add.png';
+import app_icon from './app.png';
+import youtube_icon from './youtube.png';
+import video_icon from './video.png';
+import insta_icon from './instagram.png';
+import facebook_icon from './facebook.png';
+import whatsapp_icon from './whatsapp.png';
+import slide_one from './TRoc Tools (1).png'
+import slide_two from './TRoc Tools (2).png'
+import slide_three from './TRoc Tools (3).png'
+import slide_four from './TRoc Tools (4).png'
+import slide_five from './TRoc Tools (5).png'
+import slide_six from './TRoc Tools.png';
+import slide_right from './slide-right.png';
+import slide_left from './swipe-left.png';
+import user_icon from './user.png'
+import voice_search_icon from './voice-search.png'
+import image_upload_icon from './image-upload.png';
+import text_editor_icon from './content-creator.png'
+import joke_icon from './lol.png';
+import joke2_icon from './april-fools-day.png';
+import spread_icon from './virus-transmission.png';
+import logo_icon from './logo.png';
+
+
+export const assets = {
+  large,
+  leap,
+  range,
+  shuffle,
+  sort_descending,
+  swap,
+  thermometer,
+  clc_md,
+  youtube_icon,
+  video_icon,
+  insta_icon,
+  whatsapp_icon,
+  facebook_icon,
+  slide_one,
+  slide_two,
+  slide_three,
+  slide_four,
+  slide_five,
+  slide_six,
+  app_store_icon,
+  add_icon,
+  app_icon,
+  slide_left,
+  slide_right,
+  user_icon,
+  cNet,
+  timer_icon,
+  voice_search_icon,
+  image_upload_icon,
+  text_editor_icon,
+  joke_icon,
+  joke2_icon,
+  spread_icon,
+  logo_icon
+};
